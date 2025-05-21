21b40c35a00f456023201024da5ae5a98bc1a6e7
Richard Elberger <elberger@amazon.com>
2025-05-21 14:24:21 -0400

    add comment

--- test/unit/src/test_product_provider.py
+++ test/unit/src/test_product_provider.py
@@ -77,6 +77,7 @@ class TestBulkImporter(TestCase):
             assert r == d
 
     def test_pos_lambda_handler(self):
+        """Invoke with an sqs event"""
         rcd = { "Records": [
                 {
                 "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
