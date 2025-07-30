3075b128dd229476119eca3712939ff02b628c95
Richard Elberger <elberger@amazon.com>
2025-07-30 11:41:28 -0400

    change mechanism to do coverage

---
+++ scripts/dev_coverage.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+COVERAGE_FILE=/tmp/coverage_thingpress.json
+COVERAGE_PERC=/tmp/coverage_thingpress_perc
+
+pytest --disable-socket -s \
+--cov=$(pwd)/src/bulk_importer \
+--cov=$(pwd)/src/certificate_deployer \
+--cov=$(pwd)/src/certificate_generator \
+--cov=$(pwd)/src/product_verifier \
+--cov=$(pwd)/src/provider_espressif \
+--cov=$(pwd)/src/provider_generated \
+--cov=$(pwd)/src/provider_infineon \
+--cov=$(pwd)/src/provider_microchip \
+--cov=$(pwd)/src/layer_utils \
+--cov-report=json:$COVERAGE_FILE \
+test/unit/src
+
+jq -r .totals.percent_covered_display $COVERAGE_FILE > $COVERAGE_PERC
